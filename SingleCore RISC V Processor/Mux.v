206ef607b01279c5d163947d07826e511da61117
PrasanthSirigidi <152891250+SirigidiPrasanth@users.noreply.github.com>
2025-08-31 23:41:48 +0530

    Update Mux.v

--- SingleCore RISC V Processor/Mux.v
+++ SingleCore RISC V Processor/Mux.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/01/2025 11:48:58 PM
-// Design Name: 
-// Module Name: Mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 module Mux (a,b,s,c);
 
