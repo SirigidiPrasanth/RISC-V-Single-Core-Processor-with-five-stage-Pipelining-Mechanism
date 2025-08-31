89349b478cef2f5043d074c5f43fc25580b562c9
PrasanthSirigidi <152891250+SirigidiPrasanth@users.noreply.github.com>
2025-08-31 23:46:11 +0530

    Update Control_Unit_Top.v

--- SingleCore RISC V Processor/Control_Unit_Top.v
+++ SingleCore RISC V Processor/Control_Unit_Top.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/01/2025 01:11:51 PM
-// Design Name: 
-// Module Name: Control_Unit_Top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 `include "ALU_Decoder.v"
 `include "Main_Decoder.v"
